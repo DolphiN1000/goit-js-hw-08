12ad57af5d9958c3656d6e2f489ee88a416ca9d3
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2022-12-08 23:58:44 +0200

    layer

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+    import Vimeo from "@vimeo/player"
+    console.dir(Vimeo);
+    
+    const iframe = document.querySelector('vimeo-playere');
+    console.log(iframe);
+    const player = new Vimeo.Player(iframe) ;
+    console.dir(player);
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
 
