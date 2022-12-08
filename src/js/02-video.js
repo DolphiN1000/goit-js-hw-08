85b39092ab7d6847d4e1508c0fb87a006037e57e
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2022-12-09 00:57:08 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,9 @@
     import Vimeo from "@vimeo/player"
-    console.dir(Vimeo);
+ 
     
-    const iframe = document.querySelector('vimeo-playere');
+    const iframe = document.querySelector("iframe");
     console.log(iframe);
-    const player = new Vimeo.Player(iframe) ;
+    const player = new Vimeo(iframe) ;
     console.dir(player);
     player.on('play', function() {
         console.log('played the video!');
@@ -13,4 +13,25 @@
         console.log('title:', title);
     });
 
+    // const TIME_WATCH  = 
 
+    player.setCurrentTime(50.456).then(function(seconds) {
+        // seconds = the actual time that the player seeked to
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+                break;
+    
+            default:
+                // some other error occurred
+                break;
+        }
+    });
+console.dir(player.getCurrentTime())
+    // console.log( player.getCurrentTime().then(function(seconds) {
+    //     // seconds = the current playback position
+    // }).catch(function(error) {
+    //     // an error occurred
+    // }
+    // ))
