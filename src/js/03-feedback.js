dd27d5e79499db9d56183a7846c7897cd1b977ac
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2022-12-09 19:20:43 +0200

    3task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,17 @@
+import throttle from "lodash.throttle";
+const feadBackForm = document.querySelector('.feedback-form');
+console.log(feadBackForm)
 
+
+feadBackForm.addEventListener("input", handleInput);
+let EMAIL_KEY;
+let MESSAGE_KEY;
+function handleInput (event) {
+    event.preventDefault();
+    const {
+        elements: { email, message } = event.currentTarget;
+        If (email.value === localStorage.getItem(EMAIL_KEY) {
+
+        }
+    }
+}
